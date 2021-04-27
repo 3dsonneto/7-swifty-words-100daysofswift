8231577431155861fff2166746a6bb47321bc217
Edson <edson_neto1996@hotmail.com>
2021-04-27 10:39:46 -0300

    Creating the labels and adding some properties

--- Project8/ViewController.swift
+++ Project8/ViewController.swift
@@ -9,6 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var cluesLabel: UILabel!
+    var answersLabel: UILabel!
+    var currentAnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
+    override func loadView() {
+        view = UIView() //UIView é a classe pai de todos as classes relacionadas a UIKit(labels, buttons, progressviews e etc)
+        view.backgroundColor = .white
+        
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+        
+        cluesLabel = UILabel()
+        cluesLabel.translatesAutoresizingMaskIntoConstraints = false
+        cluesLabel.font = UIFont.systemFont(ofSize: 24) //Determina que a fonte vai ter o tamanho 24 pra qualquer fonte que esteja sendo usado no ios
+        cluesLabel.text = "CLUES"
+        cluesLabel.numberOfLines = 0
+        view.addSubview(cluesLabel)
+        
+        answersLabel = UILabel()
+        answersLabel.translatesAutoresizingMaskIntoConstraints = false
+        answersLabel.font = UIFont.systemFont(ofSize: 24)
+        answersLabel.text = "ANSWERS"
+        answersLabel.numberOfLines = 0
+        view.addSubview(answersLabel)
+        
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
